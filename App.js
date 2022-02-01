d0597e433b2f1f8efde2c428a54b138035d73def
Divviya <85920511+DivviyaC@users.noreply.github.com>
2022-02-01 18:45:42 +0530

    update file

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+  TextInput,
+  AsyncStorage,
+} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+function App() {
+
+  return (
+    <View>
+      <Text>Yozyme Mobile</Text>
+    </View>
+  );
+}
+
+export default App
